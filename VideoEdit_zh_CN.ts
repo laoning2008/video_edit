986e808326fd9d5ac806bf33ba38dfccfbde2944
laoning <laoning2008@hotmail.com>
2022-11-06 09:39:33 +0800

    win

--- VideoEdit_zh_CN.ts
+++ VideoEdit_zh_CN.ts
@@ -1,3 +1,14 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN"></TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="out/build/x64-Debug/VideoEdit_autogen/include/ui_mainwindow.h" line="50"/>
+        <location filename="out/build/x86-Debug/VideoEdit_autogen/include/ui_mainwindow.h" line="50"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
